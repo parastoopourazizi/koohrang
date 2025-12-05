d99f54b2ec1e53c4826bfa981e3dc7c633b807d0
parastoopourazizi <napourazizi@gmail.com>
2025-12-05 19:13:06 +0330

    add log in button

--- src/components/header/Login.jsx
+++ src/components/header/Login.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>ورود/ثبت نام</div>
+    <div className="flex items-center space-x-2"> 
+      <button className="text-green-600 font-medium">ورود / ثبت‌نام</button>
+        <button className="text-green-600">
+          🔍
+        </button>
+     </div>
   )
 }
 
--- src/components/header/Menu.jsx
+++ src/components/header/Menu.jsx
@@ -3,27 +3,14 @@ import React from 'react'
 function Menu() {
   return (
     <div >
-        <ul className="flex items-center gap-4">
-        <li >
-            <a >صفحه‌ی اصلی</a>
-        </li>
-         <li>
-            <a>محصولات</a>
-        </li>
-         <li>
-            <a>مقالات</a>
-        </li>
-         <li>
-            <a>اخبار </a>
-        </li>
-         <li>
-            <a> توزیع و فروش</a>
-        </li>
-         <li>
-            <a> درباره ما</a>
-        </li>
-        
-        </ul></div>
+  <nav className="ml-6 space-x-4 text-green-600 font-medium">
+          <a href="/">صفحه اصلی</a>
+          <a href="/products">محصولات</a>
+          <a href="/articles">مقالات</a>
+          <a href="/news">اخبار</a>
+          <a href="/distribution">توزیع و فروش</a>
+          <a href="/about">درباره ما</a>
+        </nav></div>
   )
 }
 
